24114cbd9e7c8408d238cc7586be16fc538b3d33
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-23 20:15:03 +0300

    question 2a

--- assigment o5 OOP/Program.cs
+++ assigment o5 OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace assigment_o5_OOP
@@ -16,6 +17,9 @@ namespace assigment_o5_OOP
             //c) What is the difference between copying an object and copying its reference?
             //Copying a reference: Both variables point to the same object.
             //Copying an object: A new object is created with its own data.
+
+            //a) What is a Shallow Copy?
+            //a) Shallow Copy: Copies the object, but reference - type members still point to the same objects.
         }
 
     }
