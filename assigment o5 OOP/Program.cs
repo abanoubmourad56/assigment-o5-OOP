6ea3d3049315c84dfb8bc6d5e70e432ac1560f76
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-23 20:11:24 +0300

    question 1 a

--- assigment o5 OOP/Program.cs
+++ assigment o5 OOP/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace assigment_o5_OOP
+﻿using System.Drawing;
+
+namespace assigment_o5_OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            //a) What happens when you assign one object variable to another object variable?
+            //both variables point to the same object.
 
         }
 
