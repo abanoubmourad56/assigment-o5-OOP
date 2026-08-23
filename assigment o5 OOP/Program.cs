c5cb629b84b83a5107c6ce8d0ebf09cd1a6296ad
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-23 20:15:48 +0300

    question 2b

--- assigment o5 OOP/Program.cs
+++ assigment o5 OOP/Program.cs
@@ -20,6 +20,9 @@ namespace assigment_o5_OOP
 
             //a) What is a Shallow Copy?
             //a) Shallow Copy: Copies the object, but reference - type members still point to the same objects.
+
+            //b) What is a Deep Copy?
+            //Deep Copy: Creates a new object and also creates new copies of reference-type members.
         }
 
     }
